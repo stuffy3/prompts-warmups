88bb558da97dd2a600ccc992cb79227cb3157f45
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-01-22 13:44:41 -0700

    completed counting sheep

---
+++ If-You-Can't-Sleep-Just-Count-Sheep.js
@@ -0,0 +1,14 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: 
+// "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+let countSheep = function (num){
+    let countedSheep = ""
+    for(let i = 1; i <= num; i++) {
+      countedSheep += `${i} sheep...`
+    }
+      return countedSheep 
+  }
+  
