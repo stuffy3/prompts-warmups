70af944877bbe607c14986b874b644f177e9c61c
Tanner Stufflebeam <tannerstufflebeam@gmail.com>
2022-02-18 14:17:43 -0700

    initialized Find-The-Average-Age

---
+++ Find-The-Average-Age.js
@@ -0,0 +1,32 @@
+https://www.codewars.com/kata/582ba36cc1901399a70005fc/javascript
+
+// You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Given the following input array:
+
+// var list1 = [
+//   { firstName: 'Maria', lastName: 'Y.', country: 'Cyprus', continent: 'Europe', age: 30, language: 'Java' },
+//   { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 70, language: 'Python' },
+// ];
+// write a function that returns the average age of developers (rounded to the nearest integer). In the example above your function should return 50 (number).
+
+// Notes:
+
+// The input array will always be valid and formatted as in the example above.
+// Age is represented by a number which can be any positive integer.
+
+
+function getAverageAge(list) {
+    let arrAge = []
+    list.forEach((e) => {
+      arrAge.push(e.age)
+    })
+      return Math.round(arrAge.reduce((prev, curr) => (prev + curr), 0)/arrAge.length)  
+  }
+
+
+
+  
+  function getAverageAge(list) {
+      return Math.round(list.reduce((prev, curr) => prev + curr.age, 0 ) / list.length)
+  }
